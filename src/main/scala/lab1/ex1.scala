988173dbb2862aeacd5108f740bffb9eb02f9b4a
Wareesha ail <110236886+wareeshaali2003@users.noreply.github.com>
2023-10-01 17:05:41 +0500

    Update ex1.scala

--- src/main/scala/lab1/ex1.scala
+++ src/main/scala/lab1/ex1.scala
@@ -6,9 +6,9 @@ class Counter ( counterBits : SInt ) extends Module {
 val io = IO ( new Bundle {
 val result = Output ( Bool () )
 })
-val max = (1. U << counterBits.asUInt ) - 1. U
+val maximum = (1. U << counterBits.asUInt ) - 1. U
 val count = RegInit (0. U (16. W ) )
-when ( count === max ) {
+when ( count === maximum ) {
 count := 0. U
 } .otherwise {
 count := count + 1. U
